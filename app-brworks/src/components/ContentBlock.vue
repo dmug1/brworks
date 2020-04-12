6788a3aaff5ae20d55b3af97558582dd320f5cc3
Diego <diegomugnae@gmail.com>
2020-04-12 18:48:25 +1000

    Add more data in cards

--- app-brworks/src/components/ContentBlock.vue
+++ app-brworks/src/components/ContentBlock.vue
@@ -19,9 +19,12 @@ export default {
         name: 'contentblock',
           data: () => ({
             jobInfo: [
-              {id:1, title:'Cozinheiro',price:'$19,90', textInfo:' Fazer um bolo'},
+              {id:1, title:'Cozinhar bolo top',price:'$19,90', textInfo:' Fazer um bolo'},
               {id:2, title:'Jardineiro',price:'$189,90', textInfo:' Cortar a grama'},
               {id:3, title:'Mecanico',price:'$99,90', textInfo:' Lixar a tinta'},
+              {id:4, title:'Concertar pc',price:'$19,90', textInfo:' Fazer um bolo'},
+              {id:5, title:'Prepar orçamento',price:'$189,90', textInfo:' Cortar a grama'},
+              {id:6, title:'Limpar a vala',price:'$99,90', textInfo:' Lixar a tinta'},
             ]
         }),    
         components: {
